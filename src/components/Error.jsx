7fd06d487275cd30bd66d024a5246e97f13e62fe
Lucie Sundermeijer <lucie.sun@gmail.com>
2022-10-18 19:01:31 +0200

    add Error component style
    for desktop

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 
+import {Link} from "react-router-dom";
+
 function Error() {
     return (
-      <div> Page non trouvée !!!</div>
+      <div className="error"> 
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </div>
     )
   }
   
